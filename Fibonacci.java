13a3dc713ea7715f811567a83616fd633e4d24d3
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2024-02-04 18:22:40 +0530

    182204022024

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class Fibonacci{
     private final long[] F;
-    public Fibonacci(int max){
-        F=new long[max+1];
+    public Fibonacci(int max){//Calculations in Constructor
+        F=new long[max+1];//Always solve recurrences like this without recursion
         F[0]=0;F[1]=1;
         for(int i=2;i<=max;i++)
             F[i]=F[i-1]+F[i-2];
